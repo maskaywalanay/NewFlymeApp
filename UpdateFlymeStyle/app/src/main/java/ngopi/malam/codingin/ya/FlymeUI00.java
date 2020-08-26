a7d0b571455256876ed17c3855ce113ae6a63b2c
Maskaywalanay <mwalanay@gmail.com>
2020-08-26 22:47:39 +0700

    Fixing Onclick

--- UpdateFlymeStyle/app/src/main/java/ngopi/malam/codingin/ya/FlymeUI00.java
+++ UpdateFlymeStyle/app/src/main/java/ngopi/malam/codingin/ya/FlymeUI00.java
@@ -1,18 +1,35 @@
 package ngopi.malam.codingin.ya;
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.view.View.*;
+import android.view.*;
 
 public class FlymeUI00 extends Activity
 {
+	// Fixing Onclick ImageView
+	ImageView btn_exit0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.flyme_00);
+		setContentView(R.layout.kumpulan_flyme_00);
 		
+		btn_exit0 = (ImageView)findViewById(R.id.button_exit);
+		btn_exit0.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View p1)
+				{
+					finishAndRemoveTask();
+				}
+				
+			
+		});
 	}
+	
 	
 	
 }
